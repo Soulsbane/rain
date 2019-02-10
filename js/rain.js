f411670e0dd8b14d84c6d0bed29e547fa9ed315c
Paul Crane <paul@acheronsoft.com>
2019-02-10 01:40:56 -0600

    Make use of the particles.js library.

--- js/rain.js
+++ js/rain.js
@@ -1,19 +1,111 @@
-const PAGE_WIDTH = window.innerWidth;
-const PAGE_HEIGHT = window.innerHeight;
-const DEFAULT_DROP_AMOUNT = 100;
-
-function makeItRain(num) {
-	for(let i = 0 ; i < num ; i ++) {
-		let randomX = Math.floor(Math.random() * (PAGE_WIDTH));
-		let randomY = Math.floor(Math.random() * (PAGE_HEIGHT));
-		let dropSpeed = Math.floor(Math.random() * (25 - 5)) + 5//30;
-		let dropWidth = Math.floor(Math.random() * (dropSpeed / 10 - 1)) + 1;
-		let dropHeight = Math.floor(Math.random() * (dropSpeed * 2 - 3)) + 3;
-		let drop = new Drop(randomX, randomY, dropSpeed, dropWidth, dropHeight);
-
-		drop.show();
-		drop.fall();
-	}
-}
-
-makeItRain(DEFAULT_DROP_AMOUNT);
+particlesJS("rain",
+	{
+		"particles": {
+			"number": {
+				"value": 42,
+				"density": {
+					"enable": true,
+					"value_area": 800
+				}
+			},
+			"color": {
+				"value": "#ffffff"
+			},
+			"shape": {
+				"type": "image",
+				"stroke": {
+					"width": 0,
+					"color": "#000000"
+				},
+				"polygon": {
+					"nb_sides": 5
+				},
+				"image": {
+					"src": "images/rain.png",
+					"width": 61,
+					"height": 205
+				}
+			},
+			"opacity": {
+				"value": 0.49717086105258135,
+				"random": true,
+				"anim": {
+					"enable": false,
+					"speed": 1,
+					"opacity_min": 0.1,
+					"sync": false
+				}
+			},
+			"size": {
+				"value": 7.891600969088593,
+				"random": true,
+				"anim": {
+					"enable": false,
+					"speed": 40,
+					"size_min": 0.1,
+					"sync": false
+				}
+			},
+			"line_linked": {
+				"enable": false,
+				"distance": 150,
+				"color": "#ffffff",
+				"opacity": 0.4,
+				"width": 1
+			},
+			"move": {
+				"enable": true,
+				"speed": 27,
+				"direction": "bottom",
+				"random": false,
+				"straight": true,
+				"out_mode": "out",
+				"bounce": false,
+				"attract": {
+					"enable": false,
+					"rotateX": 600,
+					"rotateY": 1200
+				}
+			}
+		},
+		"interactivity": {
+			"detect_on": "canvas",
+			"events": {
+				"onhover": {
+					"enable": false,
+					"mode": "repulse"
+				},
+				"onclick": {
+					"enable": false,
+					"mode": "push"
+				},
+				"resize": true
+			},
+			"modes": {
+				"grab": {
+					"distance": 400,
+					"line_linked": {
+						"opacity": 1
+					}
+				},
+				"bubble": {
+					"distance": 400,
+					"size": 40,
+					"duration": 2,
+					"opacity": 8,
+					"speed": 3
+				},
+				"repulse": {
+					"distance": 200,
+					"duration": 0.4
+				},
+				"push": {
+					"particles_nb": 4
+				},
+				"remove": {
+					"particles_nb": 2
+				}
+			}
+		},
+		"retina_detect": true
+	});
