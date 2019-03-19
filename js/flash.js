3e78602321ba5f9c1f13c203542dfd930a3b24f6
Paul Crane <paul@acheronsoft.com>
2019-03-18 20:56:11 -0500

    Add flash.js.

---
+++ js/flash.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function () { initRainCanvas(200); });
+
+function initRainCanvas(interval) {
+	window.requestAnimationFrame(animate);
+	flash();
+}
+
+function animate() {
+	window.requestAnimationFrame(animate);
+}
+
+function flash() {
+	var randomTimeout = (Math.random() * 8 + 2) * 1000;
+
+	setTimeout(function () {
+		setFlashClass();
+		flash();
+	}, randomTimeout);
+}
+
+function setFlashClass() {
+	$(".flashDiv").addClass("flashing");
+	window.setTimeout(function () { $(".flashDiv").removeClass("flashing"); }, 2000);
+}
